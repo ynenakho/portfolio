00373e1893aedd48c5e27d3df7f6f3c38f26d8f9
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2021-02-16 12:34:40 -0500

    spinning logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,27 +5,25 @@ interface Props {
 const Header: React.FC<Props> = ({ title }) => {
   return (
     <div className="header">
-      {/* <div className="stage" style={{ width: 32, height: 32 }}> */}
       <div className="cubespinner">
         <div className="face1 face">
-          <img src="/logo192.png" id="face-icon" alt="logo" />
+          <img src="/icons/react-logo.png" id="face-icon" alt="logo" />
         </div>
         <div className="face2 face">
-          <img src="/redux-logo.png" id="face-icon" alt="logo" />
+          <img src="/icons/redux-logo.png" id="face-icon" alt="logo" />
         </div>
         <div className="face3 face">
-          <img src="/node-logo.png" id="face-icon" alt="logo" />
+          <img src="/icons/node-logo.png" id="face-icon" alt="logo" />
         </div>
         <div className="face4 face">
-          <img src="/github-logo.png" id="face-icon" alt="logo" />
+          <img src="/icons/github-logo.png" id="face-icon" alt="logo" />
         </div>
         <div className="face5 face">
-          <img src="/sass-logo.png" id="face-icon" alt="logo" />
+          <img src="/icons/sass-logo.png" id="face-icon" alt="logo" />
         </div>
         <div className="face6 face">
-          <img src="/python-logo.png" id="face-icon" alt="logo" />
+          <img src="/icons/python-logo.png" id="face-icon" alt="logo" />
         </div>
-        {/* </div> */}
       </div>
       <p id="title">{title}</p>
     </div>
