6691036537b8f1f25e57bad4f1315749d0b8e8cd
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2021-02-16 12:29:15 -0500

    spinning icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,24 @@ const Header: React.FC<Props> = ({ title }) => {
     <div className="header">
       {/* <div className="stage" style={{ width: 32, height: 32 }}> */}
       <div className="cubespinner">
-        <div className="face1">1</div>
-        <div className="face2">2</div>
-        <div className="face3">3</div>
-        <div className="face4">4</div>
-        <div className="face5">5</div>
-        <div className="face6">6</div>
+        <div className="face1 face">
+          <img src="/logo192.png" id="face-icon" alt="logo" />
+        </div>
+        <div className="face2 face">
+          <img src="/redux-logo.png" id="face-icon" alt="logo" />
+        </div>
+        <div className="face3 face">
+          <img src="/node-logo.png" id="face-icon" alt="logo" />
+        </div>
+        <div className="face4 face">
+          <img src="/github-logo.png" id="face-icon" alt="logo" />
+        </div>
+        <div className="face5 face">
+          <img src="/sass-logo.png" id="face-icon" alt="logo" />
+        </div>
+        <div className="face6 face">
+          <img src="/python-logo.png" id="face-icon" alt="logo" />
+        </div>
         {/* </div> */}
       </div>
       <p id="title">{title}</p>
