da8cedfc2e3cfaa9a098c8a7871d0e13385a0ca3
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2021-02-25 15:55:56 -0500

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import Router from 'Router';
 
+declare global {
+  interface Window {
+    analytics: any;
+  }
+}
+
 const App: React.FC = () => {
   useEffect(() => {
     AOS.init();
--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -6,59 +6,67 @@ interface ConnectLinkProps {
   dataAos: string;
 }
 
-const Connect: React.FC = () => {
-  const links = [
-    {
-      href: 'mailto:yuriy.nenakhov@gmail.com?subject=Hello',
-      src: 'email-icon.png',
-      dataAos: 'fade-right',
-    },
-    {
-      href: 'tel:9178626512',
-      src: 'phone-icon.png',
-      dataAos: 'fade-down-right',
-    },
-    {
-      href:
-        'https://drive.google.com/file/d/15hsNe4rEQ0wsAkuzhhGiSHIbwH7RM7xV/view?usp=sharing',
-      src: 'resume-icon.png',
-      dataAos: 'fade-down',
-    },
-    {
-      href: 'https://github.com/ynenakho',
-      src: 'github-icon.png',
-      dataAos: 'fade-down-left',
-    },
-    {
-      href: 'https://www.linkedin.com/in/yuriy-nenakhov',
-      src: 'linkedin-icon.png',
-      dataAos: 'fade-left',
-    },
-  ];
+const links = [
+  {
+    href: 'mailto:yuriy.nenakhov@gmail.com?subject=Hello',
+    src: 'email-icon.png',
+    dataAos: 'fade-right',
+  },
+  {
+    href: 'tel:9178626512',
+    src: 'phone-icon.png',
+    dataAos: 'fade-down-right',
+  },
+  {
+    href:
+      'https://drive.google.com/file/d/15hsNe4rEQ0wsAkuzhhGiSHIbwH7RM7xV/view?usp=sharing',
+    src: 'resume-icon.png',
+    dataAos: 'fade-down',
+  },
+  {
+    href: 'https://github.com/ynenakho',
+    src: 'github-icon.png',
+    dataAos: 'fade-down-left',
+  },
+  {
+    href: 'https://www.linkedin.com/in/yuriy-nenakhov',
+    src: 'linkedin-icon.png',
+    dataAos: 'fade-left',
+  },
+];
 
-  const ConnectLink: React.FC<ConnectLinkProps> = ({ href, src, dataAos }) => {
-    const target = href === 'tel:9178626512' ? '' : '_blank';
-    const rel = href === 'tel:9178626512' ? '' : 'noreferrer';
+const ConnectLink: React.FC<ConnectLinkProps> = ({ href, src, dataAos }) => {
+  const target = href === 'tel:9178626512' ? '' : '_blank';
+  const rel = href === 'tel:9178626512' ? '' : 'noreferrer';
 
-    if (src === 'resume-icon.png') {
-      return (
-        <div data-aos={dataAos} data-aos-offset="50">
-          <Link to="/resume">
-            <img src={`/icons/${src}`} alt={src} />
-          </Link>
-        </div>
-      );
-    }
-
-    return (
-      <div data-aos={dataAos} data-aos-offset="50">
-        <a href={href} target={target} rel={rel}>
-          <img src={`/icons/${src}`} alt={src} />
-        </a>
-      </div>
-    );
+  const handleClick = (target: string) => {
+    const name = target.split('-')[0];
+    window.analytics.logEvent('clickLink', { name });
   };
 
+  // if (src === 'resume-icon.png') {
+  //   return (
+  //     <div data-aos={dataAos} data-aos-offset="50" onClick={() => handleClick(src)}>
+  //       <Link to="/resume">
+  //         <img src={`/icons/${src}`} alt={src} />
+  //       </Link>
+  //     </div>
+  //   );
+  // }
+  return (
+    <div
+      data-aos={dataAos}
+      data-aos-offset="50"
+      onClick={() => handleClick(src)}
+    >
+      <a href={href} target={target} rel={rel}>
+        <img src={`/icons/${src}`} alt={src} />
+      </a>
+    </div>
+  );
+};
+
+const Connect: React.FC = () => {
   return (
     <div className="section connect">
       <h2>Connect With Me</h2>
