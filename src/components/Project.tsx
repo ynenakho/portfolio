0e2f2426edc852a7644a4073ed751d6373862070
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2021-03-24 22:20:57 -0400

    updated projects

---
+++ src/components/Project.tsx
@@ -0,0 +1,31 @@
+type Props = {
+  name: string;
+  about: string;
+  picture: string;
+  link: string;
+};
+
+const Project: React.FC<Props> = ({ name, about, picture, link }) => {
+  const handleEvent = () => {
+    window.analytics.logEvent('clickLink', { name });
+  };
+
+  return (
+    <div
+      className="project"
+      data-aos="fade-up"
+      data-aos-offset="300"
+      onClick={handleEvent}
+    >
+      <a href={link} target="_blank" rel="noreferrer">
+        <p className="project-title">{name}</p>
+        <p className="project-about">{about}</p>
+        <div className="image-wrapper">
+          <img width="100%" src={picture} alt="website screenshot" />
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,34 @@
+import Project from './Project';
+
+const projects = [
+  {
+    name: 'Camagru',
+    about: 'Instagram-like social network',
+    picture: 'pictures/camagru-website.png',
+    link: 'https://camagru-app-v2.herokuapp.com/',
+  },
+  {
+    name: 'Matcha',
+    about: 'Dating website',
+    picture: 'pictures/matcha-website.png',
+    link: 'https://matcha-sv.herokuapp.com/',
+  },
+];
+
 const Projects: React.FC = () => {
   return (
     <div className="section projects black-gradient">
       <h2 data-aos="slide-right">Projects</h2>
       <div className="projects-container">
-        <h3>Coming soon...</h3>
+        {projects.map(({ name, about, picture, link }) => (
+          <Project
+            name={name}
+            about={about}
+            picture={picture}
+            link={link}
+            key={name}
+          />
+        ))}
       </div>
     </div>
   );
